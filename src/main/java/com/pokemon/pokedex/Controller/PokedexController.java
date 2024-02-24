c60bf56679c32ea9d5c06fb89977b29c91aa8c30
서정민 <wjdals8625@gmail.com>
2024-02-24 17:24:02 +0900

    JSON 정보 가져오기 성공
    속도 진짜 엄청 빨라짐 왜 진작 이렇게 안했을꼬

--- src/main/java/com/pokemon/pokedex/Controller/PokedexController.java
+++ src/main/java/com/pokemon/pokedex/Controller/PokedexController.java
@@ -1,164 +1,33 @@
 package com.pokemon.pokedex.Controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pokemon.pokedex.DAO.PokemonDAO;
+import com.pokemon.pokedex.DAO.PokemonJSON;
 import com.pokemon.pokedex.Entity.Pokemon;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 @Controller
 public class PokedexController {
-    PokemonDAO pokeDAO = new PokemonDAO();
-    ArrayList<Pokemon> allPokemons;
-    ArrayList<Pokemon> gwandong;
-    ArrayList<Pokemon> seongdo;
-    ArrayList<Pokemon> hoyeon;
-    ArrayList<Pokemon> shinoh;
-    ArrayList<Pokemon> hana;
-    ArrayList<Pokemon> carlos;
-    ArrayList<Pokemon> alola;
-    ArrayList<Pokemon> galar;
-    ArrayList<Pokemon> paldea;
-    ArrayList<Pokemon> hisui;
     
     @GetMapping("main")
     public String goMain() throws IOException{
         return "main";
     }
 
-    @GetMapping("/gwandong")
-    public ModelAndView goGwandong() throws IOException{
+    @GetMapping("/generation/{num}")
+    public ModelAndView goJSON(@PathVariable String num) throws IOException {
         ModelAndView mav = new ModelAndView();
+
+        PokemonJSON pokeJSON = new PokemonJSON();
         
-        if(gwandong == null){
-            gwandong = pokeDAO.allList(1,151);
-        }
+        ArrayList<Pokemon> pokemons = pokeJSON.getData(num);
 
-        mav.addObject("pokemons", gwandong);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-
-    @GetMapping("/seongdo")
-    public ModelAndView goSeongdo() throws IOException{
-        ModelAndView mav = new ModelAndView();
-        
-        if(seongdo == null){
-            seongdo = pokeDAO.allList(152,251);
-        }
-
-        mav.addObject("pokemons", seongdo);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-
-    @GetMapping("/hoyeon")
-    public ModelAndView goHoyeon() throws IOException{
-        ModelAndView mav = new ModelAndView();
-        
-        if(hoyeon== null){
-            hoyeon = pokeDAO.allList(252,386);
-        }
-
-        mav.addObject("pokemons", hoyeon);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-
-    @GetMapping("/shinoh")
-    public ModelAndView goShinoh() throws IOException {
-        ModelAndView mav = new ModelAndView();
-        
-        if(shinoh == null){
-            shinoh = pokeDAO.allList(387,493);
-        }
-
-        mav.addObject("pokemons", shinoh);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-
-    @GetMapping("/hana")
-    public ModelAndView goHana() throws IOException {
-        ModelAndView mav = new ModelAndView();
-        
-        if(hana == null){
-            hana = pokeDAO.allList(494,649);
-        }
-
-        mav.addObject("pokemons", hana);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-    
-    @GetMapping("/carlos")
-    public ModelAndView goCarlos() throws IOException {
-        ModelAndView mav = new ModelAndView();
-        
-        if(carlos == null){
-            carlos = pokeDAO.allList(650,721);
-        }
-
-        mav.addObject("pokemons", carlos);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-
-    @GetMapping("/alola")
-    public ModelAndView goAlola() throws IOException {
-        ModelAndView mav = new ModelAndView();
-        
-        if(alola == null){
-            alola = pokeDAO.allList(722,809);
-        }
-
-        mav.addObject("pokemons", alola);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-
-    @GetMapping("/galar")
-    public ModelAndView goGalar() throws IOException {
-        ModelAndView mav = new ModelAndView();
-        
-        if(galar == null){
-            galar = pokeDAO.allList(810,898);
-        }
-
-        mav.addObject("pokemons", galar);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-
-    @GetMapping("/hisui")
-    public ModelAndView goHisui() throws IOException {
-        ModelAndView mav = new ModelAndView();
-        
-        if(hisui == null){
-            hisui = pokeDAO.allList(899,905);
-        }
-
-        mav.addObject("pokemons", hisui);
-        mav.setViewName("pokedex");
-        return mav;
-    }
-    
-
-    @GetMapping("/paldea")
-    public ModelAndView goPaldea() throws IOException {
-        ModelAndView mav = new ModelAndView();
-        
-        if(paldea == null){
-            paldea = pokeDAO.allList(906,1010);
-        }
-
-        mav.addObject("pokemons", paldea);
+        mav.addObject("pokemons", pokemons);
         mav.setViewName("pokedex");
         return mav;
     }
---
+++ src/main/java/com/pokemon/pokedex/DAO/PokemonJSON.java
@@ -0,0 +1,73 @@
+package com.pokemon.pokedex.DAO;
+
+import com.pokemon.pokedex.Entity.Pokemon;
+import com.pokemon.pokedex.Entity.Pokemon.Ability;
+import com.pokemon.pokedex.Entity.Pokemon.PokedexDes;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class PokemonJSON {
+    
+    public ArrayList<Pokemon> getData(String gen){
+        try (FileReader reader = new FileReader(gen+".json")) {
+            // JSON 파서 생성
+            JSONParser parser = new JSONParser();
+
+            // JSON 파일 파싱
+            JSONArray jsonArray = (JSONArray) parser.parse(reader);
+            ArrayList<Pokemon> pokemons = new ArrayList<>();
+            for (Object obj : jsonArray) {
+                ArrayList<Ability> abilities = new ArrayList<>();
+                ArrayList<PokedexDes> pokedexs = new ArrayList<>();
+                JSONObject jsonObject = (JSONObject) obj;
+                long a = (long) jsonObject.get("no");
+                int no = (int) a;
+                String korean = (String) jsonObject.get("korean");
+                String eng = (String) jsonObject.get("eng");
+                String genus = (String) jsonObject.get("genus");
+                String img = (String) jsonObject.get("img");
+                String simg = (String) jsonObject.get("simg");
+                JSONArray typesArray = (JSONArray) jsonObject.get("types");
+                JSONArray abArray = (JSONArray) jsonObject.get("abilities");
+                String[] types = new String[2];
+                for(int i=0; i<2; i++){ //포켓몬 타입 정보 가져오기
+                    types[i] = (String) typesArray.get(i);
+                }
+                for(int i=0; i<abArray.size(); i++){ //포켓몬 특성 정보 가져오기
+                    JSONObject ab = (JSONObject) abArray.get(i);
+                    String abname = (String) ab.get("name");
+                    String hidden = (String) ab.get("hidden");
+                    abilities.add(new Ability(abname,hidden));
+                }
+                JSONArray desArray = (JSONArray) jsonObject.get("des");
+                for(int i=0; i<desArray.size(); i++){
+                    JSONObject desObject = (JSONObject) desArray.get(i);
+                    String str1 = (String) desObject.get("version");
+                    String str2 = (String) desObject.get("flavor_text");
+                    if(gen.equals("9")){
+                        String[] versions = str1.split("/");
+                        String[] dess = str2.split("/");
+                        for(int j=0; j<2; j++){
+                            pokedexs.add(new PokedexDes(versions[j],dess[j]));
+                        }
+                    }else{
+                        pokedexs.add(new PokedexDes(str1,str2));
+                    }
+                    
+                }
+                pokemons.add(new Pokemon(no,korean,eng,genus,img,simg,types,abilities,pokedexs));
+            }
+            return pokemons;
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
