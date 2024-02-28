3a19d0252e649c1869d6c6230e2d023e9522c2dd
서정민 <wjdals8625@gmail.com>
2024-02-28 22:56:14 +0900

    폼체인지하는 포켓몬 도감 번호 배열 생성

--- src/main/java/com/pokemon/pokedex/Controller/PokedexController.java
+++ src/main/java/com/pokemon/pokedex/Controller/PokedexController.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 
 @Controller
 public class PokedexController {
-    int[] nums = {};
+    int[] nums = {386,413,492,487,479,351,550,555,648,641,642,645,646,647,681,720,718,741,745,801,800,849,888,889,892,483,484,905,964,978,999,901,1017,1024};
     @GetMapping("main")
     public String goMain() throws IOException{
         return "main";
