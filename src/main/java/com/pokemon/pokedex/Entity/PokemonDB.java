ca01bf21215f881c2e693518be2d6779192a584e
서정민 <wjdals8625@gmail.com>
2024-03-28 22:51:41 +0900

    PokemonDB Entity Add
    JSON과 다른 형태로 데이터를 가져와야해서 DB 전용 포켓몬 Entity 생성

---
+++ src/main/java/com/pokemon/pokedex/Entity/PokemonDB.java
@@ -0,0 +1,13 @@
+package com.pokemon.pokedex.Entity;
+
+import lombok.Data;
+
+@Data
+public class PokemonDB {
+    private int no;
+    private String name;
+    private String img;
+    private String type1;
+    private String type2;
+    private int percent;
+}
